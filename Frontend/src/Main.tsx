ca4ca8aa98ef605a28819ebd1921281ba87f3d4c
ltoppyl <toppyajibutu@gmail.com>
2022-05-16 01:00:16 +0900

    style: CSS の適用実験

---
+++ Frontend/src/Main.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+import Body from "./components/body/NewBody";
+
+const Main = () => {
+  const isDarkMode = useColorScheme() === "dark";
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}
+      >
+        <Body />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Main;
