374ca0f8643e71dbbf0e022a49a51126278e5c73
ltoppyl <toppyajibutu@gmail.com>
2022-05-17 01:09:40 +0900

    style: tailwind の適用
    様々実験するため

--- Frontend/src/components/body/Body.tsx
+++ Frontend/src/components/body/Body.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Text, TextInput, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 import axios from "axios";
 import { useTailwind } from "tailwind-rn";
 
 const Body = () => {
   const tailwind = useTailwind();
-  const [returnData, setReturnData] = useState<string>("");
-  const [inputText, setInputText] = useState<string>("");
+  const [returnData, setReturnData] = useState<string>("Before");
 
   useEffect(() => {
     const url = "http://10.0.2.2:8000/";
@@ -17,13 +16,14 @@ const Body = () => {
         setReturnData(res.data);
       })
       .catch((error) => {
-        console.error(error);
         setReturnData("Backend Error");
+        console.error(error);
       });
   }, []);
 
   return (
     <>
+      {/* CSS を使った文字の出力実験 */}
       <Text style={tailwind("text-pink-600 text-center")}>{returnData}</Text>
 
       <View style={tailwind("pt-3 items-center")}>
@@ -34,6 +34,7 @@ const Body = () => {
         </View>
       </View>
 
+      {/* CSS を使った文字の入力実験 */}
       <View>
         <Text style={tailwind("text-sm font-medium text-gray-700")}>Price</Text>
         <View style={tailwind("mt-1 relative rounded-md")}>
@@ -56,20 +57,6 @@ const Body = () => {
             )}
             placeholder="0.00"
           />
-          {/* <View className="absolute inset-y-0 right-0 flex items-center">
-            <Text htmlFor="currency" className="sr-only">
-              Currency
-            </Text>
-            <select
-              id="currency"
-              name="currency"
-              className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md"
-            >
-              <option>USD</option>
-              <option>CAD</option>
-              <option>EUR</option>
-            </select>
-          </View> */}
         </View>
       </View>
     </>
