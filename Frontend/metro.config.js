dcd852b40480373f82ebc138b4247563e1ed24c9
ltoppyl <toppyajibutu@gmail.com>
2022-05-18 10:22:33 +0900

    chore: 絶対で指定できるように変更

--- Frontend/metro.config.js
+++ Frontend/metro.config.js
@@ -1,11 +1,11 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const path = require("path");
 
 module.exports = {
+  resolver: {
+    extraNodeModules: {
+      src: path.resolve(__dirname, "src"),
+    },
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
