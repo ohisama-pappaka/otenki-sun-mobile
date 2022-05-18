bf76405c01f51fe290c23d46f3a4249f8afb2493
ltoppyl <toppyajibutu@gmail.com>
2022-05-18 10:30:46 +0900

    refactor: パスを絶対パス変更

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { TailwindProvider } from "tailwind-rn";
-import utilities from "./tailwind.json";
+// XXX: 警告が出ていることは確認している
+import utilities from "./tailwind";
 
-import Main from "./src/Main";
+import Main from "src/Main";
 
 const App = () => (
   // XXX: Error を出してるが解消方法がわからない。ただ普通に動く
