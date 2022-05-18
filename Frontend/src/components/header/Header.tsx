254cb8b38e2e004af9a56eebc1955cf27c50e1f4
ltoppyl <toppyajibutu@gmail.com>
2022-05-18 10:37:36 +0900

    feat: header のベタ書き

--- Frontend/src/components/header/Header.tsx
+++ Frontend/src/components/header/Header.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { Text, TextInput, View } from "react-native";
+import { Text, View } from "react-native";
+import { useTailwind } from "tailwind-rn";
 
 const Header = () => {
-  return <Text>Header</Text>;
+  const tailwind = useTailwind();
+  return (
+    <View style={tailwind("h-14 bg-sky-500")}>
+      <Text style={tailwind("p-4 text-slate-50 text-center")}>Header</Text>
+    </View>
+  );
 };
 
 export default Header;
