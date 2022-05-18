25d90f45fb73cba230eaa4984f41a5ca05effed4
ltoppyl <toppyajibutu@gmail.com>
2022-05-18 10:15:50 +0900

    refactor: 作業用の部分の整理

--- Frontend/src/components/body/Body.tsx
+++ Frontend/src/components/body/Body.tsx
@@ -1,17 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { Text, TextInput, View } from "react-native";
-import { Calendar } from "react-native-calendars";
+import React from "react";
+import { Text } from "react-native";
 import { useTailwind } from "tailwind-rn";
-import axios from "axios";
 
 const Body = () => {
   const tailwind = useTailwind();
-  const [returnData, setReturnData] = useState<string>("Before");
 
   return (
     <>
       {/* CSS を使った文字の出力実験 */}
-      <Text style={tailwind("text-pink-600 text-center")}>{returnData}</Text>
+      <Text style={tailwind("text-pink-600 text-center")}>Body</Text>
     </>
   );
 };
