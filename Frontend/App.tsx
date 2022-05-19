7bd54d1b982f8903735efca0be902dc4df3184ca
ltoppyl <toppyajibutu@gmail.com>
2022-05-19 22:22:37 +0900

    style: ヘッダーの背景色の追加

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -16,7 +16,14 @@ const App = () => (
   // XXX: Error を出してるが解消方法がわからない。ただ普通に動く
   <TailwindProvider utilities={utilities}>
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#0EA5E9",
+          },
+        }}
+      >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="DateRegistration" component={DateRegistration} />
         <Stack.Screen name="PointRegistration" component={PointRegistration} />
