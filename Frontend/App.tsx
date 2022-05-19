ccfc8f94df505d8b4c77b469217119e5a7f85d28
ltoppyl <toppyajibutu@gmail.com>
2022-05-19 13:54:15 +0900

    feat: 画面遷移の簡単な動きの実装
    ルーティングを確認するために簡単なものを実装

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import { TailwindProvider } from "tailwind-rn";
 // XXX: 警告が出ていることは確認している
 import utilities from "./tailwind";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Main from "src/Main";
+import SettingTime from "src/SettingTime";
+
+const Stack = createStackNavigator();
 
 const App = () => (
   // XXX: Error を出してるが解消方法がわからない。ただ普通に動く
   <TailwindProvider utilities={utilities}>
-    <Main />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="SettingTime" component={SettingTime} />
+      </Stack.Navigator>
+    </NavigationContainer>
   </TailwindProvider>
 );
 
--- Frontend/src/components/footer/Footer.tsx
+++ Frontend/src/components/footer/Footer.tsx
@@ -1,12 +1,21 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 import { useTailwind } from "tailwind-rn";
 
 const Footer = () => {
   const tailwind = useTailwind();
+  const navigation = useNavigation();
+
   return (
     <View style={tailwind("h-full bg-sky-500")}>
-      <Text style={tailwind("p-5 text-slate-50 text-center")}>Footer</Text>
+      <Text
+        style={tailwind("p-5 text-slate-50 text-center")}
+        // XXX: 警告が出ていることは確認している
+        onPress={() => navigation.navigate("SettingTime")}
+      >
+        Footer
+      </Text>
     </View>
   );
 };
