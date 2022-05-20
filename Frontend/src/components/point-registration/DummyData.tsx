b11e34e51e984e99bfb1b2bde39251b4f92849b7
ltoppyl <toppyajibutu@gmail.com>
2022-05-20 18:58:23 +0900

    feat: ファイルの作成

---
+++ Frontend/src/components/point-registration/DummyData.tsx
@@ -0,0 +1,17 @@
+export const dummyDataPrefecture = [
+  { label: "北海道", value: "北海道" },
+  { label: "青森", value: "青森" },
+  { label: "岩手", value: "岩手" },
+  { label: "宮城", value: "宮城" },
+  { label: "秋田", value: "秋田" },
+  { label: "山形", value: "山形" },
+];
+
+export const dummyDataCity = [
+  { label: "札幌市", value: "札幌市" },
+  { label: "函館市", value: "函館市" },
+  { label: "小樽市", value: "小樽市" },
+  { label: "旭川市", value: "旭川市" },
+  { label: "室蘭市", value: "室蘭市" },
+  { label: "釧路市", value: "釧路市" },
+];
