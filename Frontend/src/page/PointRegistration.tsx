ac5093a0105e337663298e2eeb524f4bb6c5b319
ltoppyl <toppyajibutu@gmail.com>
2022-05-20 14:41:37 +0900

    refactor: ファイル分割

--- Frontend/src/page/PointRegistration.tsx
+++ Frontend/src/page/PointRegistration.tsx
@@ -3,27 +3,22 @@ import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  Text,
   useColorScheme,
-  View,
-  Modal,
-  Pressable,
 } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import { useTailwind } from "tailwind-rn";
 import { useNavigation } from "@react-navigation/native";
 
 import SelectPoint from "src/components/point-registration/SelectPoint";
+import Confirmation from "src/components/point-registration/Confirmation";
+
 const PointRegistration = () => {
   const isDarkMode = useColorScheme() === "dark";
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const tailwind = useTailwind();
-  const navigation = useNavigation();
   const [modalVisible, setModalVisible] = useState<boolean>(false);
-
   const [selectedPrefecture, setSelectedPrefecture] = useState<string>("");
   const [selectedCity, setSelectedCity] = useState<string>("");
 
@@ -33,13 +28,6 @@ const PointRegistration = () => {
       setModalVisible(true);
     }
   }, [selectedPrefecture, selectedCity]);
-
-  // キャンセル時の入力データのクリア
-  const onRefresh = useCallback(() => {
-    setModalVisible(false);
-    setSelectedPrefecture("");
-    setSelectedCity("");
-  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -55,52 +43,14 @@ const PointRegistration = () => {
           setSelectedCity={setSelectedCity}
         />
 
-        <View style={tailwind("flex-1 justify-center items-center mt-20")}>
-          <Modal
-            animationType="slide"
-            transparent={true}
-            visible={modalVisible}
-          >
-            <View style={tailwind("flex-1 justify-center items-center mt-20")}>
-              <View
-                style={tailwind(
-                  "m-20 bg-white rounded-lg p-0 items-center px-4 py-10"
-                )}
-              >
-                <Text style={tailwind("mb-4 text-center")}>
-                  {selectedPrefecture}
-                  {selectedCity}
-                  を登録しますか？
-                </Text>
-                <View style={tailwind("flex flex-row")}>
-                  <Pressable
-                    style={tailwind(
-                      "rounded-lg border-2 border-gray-500 p-2 m-2 bg-white"
-                    )}
-                    // XXX: 警告が出ていることは確認している
-                    onPress={() => navigation.navigate("Home")}
-                  >
-                    <Text style={tailwind("font-bold text-center")}>
-                      登録する
-                    </Text>
-                  </Pressable>
-                  <Pressable
-                    style={tailwind(
-                      "rounded-lg border-2 border-red-600 p-2 m-2 bg-red-600"
-                    )}
-                    onPress={() => {
-                      onRefresh();
-                    }}
-                  >
-                    <Text style={tailwind("text-white font-bold text-center")}>
-                      キャンセル
-                    </Text>
-                  </Pressable>
-                </View>
-              </View>
-            </View>
-          </Modal>
-        </View>
+        <Confirmation
+          selectedPrefecture={selectedPrefecture}
+          setSelectedPrefecture={setSelectedPrefecture}
+          selectedCity={selectedCity}
+          setSelectedCity={setSelectedCity}
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+        />
       </ScrollView>
     </SafeAreaView>
   );
