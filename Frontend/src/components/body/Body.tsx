0290679c2cbb79865a0e96d8ca6253f15d562d53
ltoppyl <toppyajibutu@gmail.com>
2022-05-20 21:26:22 +0900

    feat: その日の天気の UI の作成

--- Frontend/src/components/body/Body.tsx
+++ Frontend/src/components/body/Body.tsx
@@ -1,16 +1,31 @@
 import React from "react";
-import { View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { useTailwind } from "tailwind-rn";
 
 const Body = () => {
   const tailwind = useTailwind();
+  const sunnyImagePath = "src/img/sunny.png";
+  const highestTemperature = 30;
+  const lowestTemperature = -40;
 
   return (
     <>
-      <View style={tailwind("h-40 bg-gray-100")} />
-      <View style={tailwind("h-40 bg-gray-200")} />
-      <View style={tailwind("h-40 bg-gray-300")} />
-      <View style={tailwind("h-40 bg-gray-400")} />
+      <View style={tailwind("h-52 rounded-md p-4 m-4")}>
+        <Text style={tailwind("text-center text-2xl")}>東京</Text>
+        <Image
+          style={tailwind("w-24 h-24 self-center")}
+          source={require(sunnyImagePath)}
+        />
+        <Text style={tailwind("text-center text-amber-500")}>
+          最高気温 {highestTemperature}℃
+        </Text>
+        <Text style={tailwind("text-center text-blue-600")}>
+          最低気温 {lowestTemperature}℃
+        </Text>
+        <Text style={tailwind("text-center")}>
+          降水確率(午前) / 降水確率(午後)
+        </Text>
+      </View>
     </>
   );
 };
