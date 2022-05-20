3ceca7a61c9d9ed5f9aa073aeec957be91b6bd15
Kensuke Fukami <kensuke8293@gmail.com>
2022-05-20 20:08:39 +0900

    feat:地点、予定日への遷移

--- Frontend/src/page/Others.tsx
+++ Frontend/src/page/Others.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import {
   SafeAreaView,
@@ -7,9 +8,12 @@ import {
   useColorScheme,
 } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
+import { useTailwind } from "tailwind-rn";
 
 const Others = () => {
   const isDarkMode = useColorScheme() === "dark";
+  const tailwind = useTailwind();
+  const navigation = useNavigation();
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -21,7 +25,21 @@ const Others = () => {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}
       >
-        <Text>遷移したよ！</Text>
+        <Text>Q&A</Text>
+        <Text
+          style={tailwind("p-5 text-currunt text-center")}
+          // XXX: 警告が出ていることは確認している
+          onPress={() => navigation.navigate("PointRegistration")}
+        >
+          地点の追加/変更/削除
+        </Text>
+        <Text
+          style={tailwind("p-5 text-currunt text-center")}
+          // XXX: 警告が出ていることは確認している
+          onPress={() => navigation.navigate("DateRegistration")}
+        >
+          予定日の追加/変更/削除
+        </Text>
       </ScrollView>
     </SafeAreaView>
   );
