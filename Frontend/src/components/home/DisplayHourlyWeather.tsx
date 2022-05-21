efda180954e502125008285336be1406ae53fe7e
ltoppyl <toppyajibutu@gmail.com>
2022-05-21 10:39:29 +0900

    refactor: 不要なモジュールの削除

--- Frontend/src/components/home/DisplayHourlyWeather.tsx
+++ Frontend/src/components/home/DisplayHourlyWeather.tsx
@@ -1,29 +1,7 @@
 import React from "react";
-import {
-  Alert,
-  Image,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { Image, View } from "react-native";
 import { useTailwind } from "tailwind-rn";
-import {
-  Table,
-  Row,
-  Rows,
-  TableWrapper,
-  Cell,
-} from "react-native-table-component";
-
-const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: "#fff" },
-  head: { height: 40, backgroundColor: "#808B97" },
-  text: { margin: 6 },
-  row: { flexDirection: "row", backgroundColor: "#FFF1C1" },
-  btn: { width: 58, height: 18, backgroundColor: "#78B7BB", borderRadius: 2 },
-  btnText: { textAlign: "center", color: "#fff" },
-});
+import { Table, Row, TableWrapper, Cell } from "react-native-table-component";
 
 const DisplayHourlyWeather = () => {
   const tailwind = useTailwind();
